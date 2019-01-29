fde19e08acc2034efac185f2db1283e5f80b8232
unknown <retheesh@TENT-Retheesh.tamdeenrealestate.com>
2019-01-29 21:06:53 +0300

    Nabil & Retheesh Update1

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render, redirect
 from .models import Restaurant
-from .forms import RestaurantForm
+from .forms import RestaurantForm,SignupForm
+from django.contrib.auth import login
 
 def signup(request):
-    
+    form=SignupForm()
+    if request.method='POST':
+        form=SignupForm(request,POST)
+        if form.is_valid():
+            user=form.save(commit=False)
+            user.set_password(user.password)
+            user.save()
+
+            login(request,user)
+            return redirect("restaurant-list")
+    context={
+        "form":form,
+    }
+
     return render(request, 'signup.html', context)
 
 def signin(request):
